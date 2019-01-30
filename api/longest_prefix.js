fc36f2d589c895c13d0b79f332610fb7d778821e
Chris W. Dempewolf <cdempewolf@integralgis.local>
2019-01-30 09:45:39 -0800

    removing extraneous files part deux

--- api/longest_prefix.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-    app = express();
-
-app.get('/node/longest_prefix/api/fetchPrefix', function (req, res) {
-    res.send({ items: ["abc", "abcd", "abccd", "abcde", "abcdde", "abcdee"] });
-});
-
-app.set('port', process.env.PORT);
-app.listen(process.env.PORT);
